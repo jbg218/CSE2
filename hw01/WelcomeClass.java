fad59e651116843f7d1bda7deaffc4223271ba13
jbg218 <jbg218@lehigh.edu>
2015-08-30 01:11:28 +0000

    Homework 1

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+//Jeremy Glennon
+//Welcome Class Homework 01
+//compile program
+//  javac WelcomeClass.java
+//run the program
+//  java WelcomeClass
+
+// define a class
+public class WelcomeClass
+{
+// add main method
+    public static void main(String[] args) 
+    {
+//print the statement
+    System.out.println("   -----------");
+    System.out.println("   | WELCOME |");
+    System.out.println("   -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("<-J--B--G--2--1--8->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("I play Ultimate Frisbee on Lehigh's club team");
+    }
+}
