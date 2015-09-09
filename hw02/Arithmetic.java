3735bc4433fdec8dac0c0bf0fc0b71b1abbff29d
jbg218 <jbg218@lehigh.edu>
2015-09-09 00:34:18 +0000

    updated hw02

--- hw02/Arithmetic.java
+++ hw02/Arithmetic.java
@@ -22,34 +22,55 @@ public class Arithmetic
         double totalSockCost$;
         double totalGlassesCost$;
         double totalEnvelopesCost$;
-        double preTaxTotal;
-        double taxTotal;
-        double postTaxTotal;
-//Multiply the price by the amount to get the cost of each item
+        double taxSocks$;
+        double taxGlasses$;
+        double taxEnvelopes$;
+        double preTaxTotal$;
+        double taxTotal$;
+        double postTaxTotal$;
+//Multiply the price by the amount to get the cost and tax of each item
         totalSockCost$=nSocks*sockCost$;
         totalGlassesCost$=nGlasses*glassesCost$;
         totalEnvelopesCost$=nEnvelopes*envelopeCost$;
-//add all the costs to get the total before taxes
-        preTaxTotal=totalEnvelopesCost$+totalGlassesCost$+totalSockCost$; 
-//multiply the total by 1 plus the tax rate to get the total with tax
-        postTaxTotal=preTaxTotal*1.06;
-//multiply the total by the tax rate to get the total tax
-        taxTotal= preTaxTotal*0.06;
+        taxEnvelopes$=envelopeCost$*taxPercent*nEnvelopes;
+        taxGlasses$=glassesCost$*taxPercent*nGlasses;
+        taxSocks$=sockCost$*taxPercent*nSocks;
+        //add all the item costs to get the total before taxes
+        preTaxTotal$=totalEnvelopesCost$+totalGlassesCost$+totalSockCost$; 
+//Add the total for each item plus its tax to get total cost
+        postTaxTotal$=totalSockCost$+totalGlassesCost$+totalEnvelopesCost$+taxEnvelopes$+taxGlasses$+taxSocks$;
+//Add all the taxes together to get the total tax
+        taxTotal$= taxEnvelopes$+taxGlasses$+taxSocks$;
 //multiply the doubles by 100 then convert to int
-//convert back to double then divide by 100
-//this gives a better decimal value than leaving the double
-        preTaxTotal= (int) (preTaxTotal*100);
-        postTaxTotal= (int) (postTaxTotal*100);
-        taxTotal= (int) (taxTotal*100);
-        preTaxTotal= (double) (preTaxTotal/100);
-        postTaxTotal= (double) (postTaxTotal/100);
-        taxTotal= (double) (taxTotal/100);
-//Print out the total for each item as well as the total before and after tax and the tax itself
-        System.out.println("The cost for socks is $"+totalSockCost$+".");
-        System.out.println("The cost for glasses is $"+totalGlassesCost$+".");
-        System.out.println("The cost for envelopes is $"+totalEnvelopesCost$+".");
-        System.out.println("The total before tax is $"+preTaxTotal+".");
-        System.out.println("The tax is $"+taxTotal+".");
-        System.out.println("The total with tax is $"+postTaxTotal+".");
+//convert back to double after dividing by 100
+//this gives a less precise but more user friendly decimal value than leaving the double
+        preTaxTotal$= (int) (preTaxTotal$*100);
+        postTaxTotal$= (int) (postTaxTotal$*100);
+        taxTotal$= (int) (taxTotal$*100);
+        taxSocks$= (int) (taxSocks$*100);
+        taxGlasses$= (int) (taxGlasses$*100);
+        taxEnvelopes$= (int) (taxEnvelopes$*100);
+        preTaxTotal$= (double) (preTaxTotal$/100);
+        postTaxTotal$= (double) (postTaxTotal$/100);
+        taxTotal$= (double) (taxTotal$/100);
+        taxSocks$= (double) (taxSocks$/100);
+        taxGlasses$= (double) (taxGlasses$/100);
+        taxEnvelopes$= (double) (taxEnvelopes$/100);
+//Print out the number of each item being bought and its price
+//Also print out the total for each item 
+//Print out the tax for each item
+//as well as the total before and after tax and the total tax itself
+        System.out.println(nSocks+" packs of socks are being bought at "+sockCost$+" per pack.");
+        System.out.println(nGlasses+" glasses are being bought at $"+glassesCost$+" per glass.");
+        System.out.println(+nEnvelopes+" envelope is being bought at $"+envelopeCost$+" per envelope.");
+        System.out.println("The cost for "+nSocks+" packs of socks is $"+totalSockCost$+".");
+        System.out.println("The cost for "+nGlasses+" glasses is $"+totalGlassesCost$+".");
+        System.out.println("The cost for "+nEnvelopes+" envelope is $"+totalEnvelopesCost$+".");
+        System.out.println("The tax for the socks is $"+(taxSocks$)+".");
+        System.out.println("The tax for the glasses is $"+(taxGlasses$)+".");
+        System.out.println("The tax for the envelope is $"+(taxEnvelopes$)+".");
+        System.out.println("The total before tax is $"+preTaxTotal$+".");
+        System.out.println("The total tax is $"+taxTotal$+".");
+        System.out.println("The total with tax is $"+postTaxTotal$+".");
     }
 }
